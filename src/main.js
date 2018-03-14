6b2a94786f4ac7b59a04ac392793aac4f50ec5e2
theoleanse <theoleanse@gmail.com>
2018-03-14 15:58:07 +0000

    add vid

--- src/main.js
+++ src/main.js
@@ -1,38 +1,73 @@
-const texts = [
-	'jung',
-	'paterson',
-	'sicp',
-	'st-julian',
-	'the-dead'
+const content = [
+	{
+		id: 'jung',
+		type: 'text'
+	},
+	{
+		id: 'paterson',
+		type: 'text'
+	},
+	{
+		id: 'st-julian' ,
+		type: 'text'
+	},
+	{
+		id: 'the-dead',
+		type: 'text'
+	},
+	{
+		id: '2Op3QLzMgSY',
+		type: 'video'
+	}
 ];
 
 const order = [];
-while (order.length < texts.length) {
-	const randomIndex = Math.floor(Math.random() * texts.length)
+while (order.length < content.length) {
+	const randomIndex = Math.floor(Math.random() * content.length)
 	if (!order.includes(randomIndex)) {
 		order.push(randomIndex)
 	}
 }
 
 const main = document.querySelector('.js-main');
-order.forEach(async index => {
-	const text = await fetch(`http://theoleanse.com/RlVDSw-/texts/${texts[index]}.txt`).then(res => res.text());
+const els = Promise.all(order.map(async index => {
+	const item = content[index];
 	const el = document.createElement('div');
-	el.innerText = text;
 	el.classList.add(
-		'js-text',
+		'js-content-item',
 		'measure',
 		'lh-copy',
 		'f5', 'f3-ns',
 		'pv4',
 		'bb'
 	)
-	main.appendChild(el);
-})
+
+	if (item.type === 'text') {
+		el.innerText = await fetch(`http://theoleanse.com/RlVDSw-/texts/${item.id}.txt`).then(res => res.text());
+	} else if (item.type === 'video') {
+		el.innerHTML = `<iframe
+			width="100%"
+			height="400"
+			src="https://www.youtube.com/embed/${item.id}"
+			frameborder="0"
+			allow="autoplay; encrypted-media"
+			allowfullscreen/>`;
+	}
+
+	return el;
+}))
+	.then(els =>
+		els.forEach(el =>
+			main.appendChild(el)));
+
 
 const nextButton = document.querySelector('.js-next')
 nextButton.addEventListener('click', () => {
-	const textsOnPage = document.querySelectorAll('.js-text');
-	const nextText = [...textsOnPage].find(text => text.getBoundingClientRect().bottom > 0).nextSibling;
-	nextText ? nextText.scrollIntoView() : textsOnPage[0].scrollIntoView();
+	const contentItems = document.querySelectorAll('.js-content-item');
+	const next = [...contentItems].find(text =>
+		text.getBoundingClientRect().bottom > 0)
+			.nextSibling
+	next && (next.getBoundingClientRect().bottom > window.innerHeight)
+		? next.scrollIntoView()
+		: contentItems[0].scrollIntoView();
 })
